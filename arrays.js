d28b9a42ca5dc6822a915c2aeef2dba458a4b77e
mateoyamith <mateosanchez9602@gmail.com>
2021-01-25 16:15:57 -0500

    Arrays

---
+++ arrays.js
@@ -0,0 +1,15 @@
+var frutas = ["Manzana", "Platano", "Cereza", "Fresa"];
+
+console.log(frutas);
+console.log(frutas.length);
+console.log(frutas[3]);
+
+var masFrutas = frutas.push("Uvas");
+
+var ultimo = frutas.pop("Uvas");
+
+var nuevaLongitud = frutas.unshift("Uvas");
+
+var borrarFruta = frutas.shift("Uvas");
+
+var posicion = frutas.indexOf("Cereza");
