9ebb426e5687d0a56966b771d9017690cd6721bb
mateoyamith <mateosanchez9602@gmail.com>
2021-01-25 12:24:51 -0500

    Funciones

---
+++ funciones.js
@@ -0,0 +1,15 @@
+/*Declarativas*/
+function miFuncion() {
+    return 3;
+}
+
+miFuncion();
+
+/*Expresion*/
+
+//Funcion anonima
+var miFuncion2 = function(a, b) {
+    return a + b;
+}
+
+miFuncion2(1, 2);
