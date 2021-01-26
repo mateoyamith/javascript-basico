6b5a0684ba905e88be470a6bce5782f6124fde51
mateoyamith <mateosanchez9602@gmail.com>
2021-01-25 21:43:38 -0500

    Objects

---
+++ objects.js
@@ -0,0 +1,8 @@
+var miAuto = {
+    marca: "Toyota",
+    modelo: "Corolla",
+    annio: 2021,
+    detalleDelAuto: function() {
+        console.log(`Auto ${this.modelo} ${this.annio}`);
+    }
+};
