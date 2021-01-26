d8c4148fbb19fd79534a1031b9299ea9e4dc1d62
mateoyamith <mateosanchez9602@gmail.com>
2021-01-25 21:48:45 -0500

    Objects: Función constructora

--- objects.js
+++ objects.js
@@ -6,3 +6,14 @@ var miAuto = {
         console.log(`Auto ${this.modelo} ${this.annio}`);
     }
 };
+
+/*Funcion constructora*/
+function auto(marca, modelo, annio) {
+    this.marca = marca;
+    this.modelo = modelo;
+    this.annio = annio;
+}
+
+var autoNuevo = new auto("Tesla", "Model 3", 2021);
+var autoNuevo2 = new auto("Mazda", "CR-X", 2021);
+var autoNuevo3 = new auto("Chevrolet", "Onix", 2021);
